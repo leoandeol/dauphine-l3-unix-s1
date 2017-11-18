a471b6e90e3eaa351ac5dfbbdd221ce309b96afd
lamyaa <lamyaarizki1996@gmail.com>
2017-11-18 13:29:20 +0100

    commit

--- java/src/Passwd.java
+++ java/src/Passwd.java
@@ -1,91 +1,104 @@
 import java.util.Scanner;
-
-import static java.util.Collections.replaceAll;
-
 public class Passwd {
 
-        public static final String PROGRAM_NAME = "Passwd" ;
+    public static final String PROGRAM_NAME = "Passwd" ;
 
-        /**
-         * The size of the buffer to be used for reading from the
-         * file.  A buffer of this size is filled before writing
-         * to the output file.
-         */
-        public static final int BUF_SIZE = 4096 ;
+    /**
+     * The size of the buffer to be used for reading from the
+     * file.  A buffer of this size is filled before writing
+     * to the output file.
+     */
+    public static final int BUF_SIZE = 4096 ;
 
-        /**
-         * The file mode to use when creating the output file.
-         */
-        public static final short OUTPUT_MODE = 0700 ;
+    /**
+     * The file mode to use when creating the output file.
+     */
+    public static final short OUTPUT_MODE = 0700 ;
 
-        /**
-         * Copies standard input to standard output and to a file.
-         * @exception java.lang.Exception if an exception is thrown
-         * by an underlying operation
-         */
-        public static void main( String[] argv ) throws Exception
+    /**
+     * Copies standard input to standard output and to a file.
+     * @exception java.lang.Exception if an exception is thrown
+     * by an underlying operation
+     */
+    public static void main( String[] argv ) throws Exception
+    {
+        // initialize the file system simulator kernel
+        Kernel.initialize() ;
+
+        // print a helpful message if the number of arguments is not correct
+        if( argv.length != 1 )
         {
-            // initialize the file system simulator kernel
-            Kernel.initialize() ;
+            System.err.println( PROGRAM_NAME + ": usage: java " + PROGRAM_NAME +
+                    " output-file" ) ;
+            Kernel.exit( 1 ) ;
+        }
 
-            // print a helpful message if the number of arguments is not correct
-            if( argv.length != 1 )
-            {
-                System.err.println( PROGRAM_NAME + ": usage: java " + PROGRAM_NAME +
-                        " output-file" ) ;
-                Kernel.exit( 1 ) ;
-            }
+        // give the command line argument a better name
+        String name = argv[0] ;
 
-            // give the command line argument a better name
-            String name = argv[0] ;
+        //ask user for his old password
+        String opw="";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your old  password  :");
+        opw = sc.next();
+        //open the file /etc/shadow
+        int out_fd = Kernel.open( "/etc/shadow",OUTPUT_MODE) ;
+        if( out_fd < 0 )
+        {
+            Kernel.perror( PROGRAM_NAME ) ;
+            System.err.println( PROGRAM_NAME +
+                    ": error during write to output file" ) ;
+            Kernel.exit( 2 ) ;
+        }
 
-            //ask user for his new password
-            String mp="",mpc="";
+        //read the file in a table of string
+        int rd;
+        boolean exist=false;
+        do {
+            byte[] ch = new byte[1];
+            String line = "";
             do {
-                Scanner sc = new Scanner(System.in);
-                System.out.println("enter you password  :");
-                mp = sc.next();
-                System.out.println("confirm your password  :");
-                mpc = sc.next();
-            }while(!mp.equals(mpc));
-            //open the file /etc/shadow
-                int out_fd = Kernel.open( "/etc/shadow",OUTPUT_MODE) ;
-                if( out_fd < 0 )
-                {
-                    Kernel.perror( PROGRAM_NAME ) ;
-                    System.err.println( PROGRAM_NAME +
-                            ": error during write to output file" ) ;
-                    Kernel.exit( 2 ) ;
+                rd =Kernel.read(out_fd, ch, 1);
+                String str = new String(ch);
+                line += str;
+            } while (ch[0] != 0x3A);
+            String[] parts = line.split(":", 7);
+            String user = parts[0];
+            String password = parts[1];
+            if(password==opw && user==name){ exist=true; break;}//as we find the user we  do  break
+        }while(rd>0);
+        if(exist) {
+            String pw = "", pwc = "";
+            Scanner s = new Scanner(System.in);
+            System.out.println("enter your password  :");
+            pw = s.next();
+            System.out.println("confirm your password  :");
+            pwc = s.next();
+            if (!pw.equals(pwc)) System.out.println("please make sure you entered the same password");
+            else{
+                //when we did the break was the last position in the file!!!!!!!!!!!!
+                Kernel.lseek(out_fd,0,1);
+                String line =name+":x:0:0:0:0::";
+                String newline =name+":"+pw+":0:0:0:0::";
+                int wr_count = Kernel.write(out_fd, line.replace(line,newline).getBytes(), newline.getBytes().length);
+                if (wr_count <= 0) {
+                    Kernel.perror(PROGRAM_NAME);
+                    System.err.println(PROGRAM_NAME +
+                            ": error during write to output file");
+                    Kernel.exit(3);
                 }
-                //upload the password of user
-                int i = Kernel.searchword(out_fd,name);
-                int count=0;
-                if(i!=-1){
-                    do {
-                        int rd=0;
-                        byte[] ch=new byte[0];
-                        rd = Kernel.read(out_fd, ch, 1);
-                        if(ch[0]==0x0A)
-                            count++;
-                    } while (i!=count);
-                    Kernel.lseek(out_fd,0,1);
-                    String line =name+"::0:0:0:0::";
-                    String newline =name+":"+mp+":0:0:0:0::";
-                    int wr_count = Kernel.write(out_fd, line.replace(line,newline).getBytes(), newline.getBytes().length);
-                    if (wr_count <= 0) {
-                        Kernel.perror(PROGRAM_NAME);
-                        System.err.println(PROGRAM_NAME +
-                                ": error during write to output file");
-                        Kernel.exit(3);
-                    }
+                System.out.println("your password is successfully modified");}
+        }
+        else  System.out.println("this user doesn't exist or the password is not correct");
 
-                    // close the output file
-                    Kernel.close(out_fd);
+        // close the output file
+        Kernel.close(out_fd);
 
-                    // exit with success
-                    Kernel.exit(0);
-                }
+         /* exit with success */
+        Kernel.exit(0);
+    }
 
-    }}
+}
+
 
 
